738c13fe3960dafd8cfd3fd15064e32e21ddef9f
Felix Rossmann <felix.k.rossmann@gmail.com>
2019-03-14 22:08:18 +0100

    [CHANGE] Add base structure of project.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+
+var autoPrefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    gulpSass = require('gulp-sass');
+
+gulp.task('sass-default', function () {
+    return gulp.src('src/scss/tiles.scss')
+        .pipe(gulpSass())
+        .pipe(autoPrefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass-watch', function () {
+    return gulp.watch('src/scss/*.scss', gulp.series('sass-default'));
+});
